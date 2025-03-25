cd675051df71c77206eec3f7b3ac18a45740794d
hadar <hadargerashi2005@gmail.com>
2025-03-25 23:47:05 +0200

    use .env

--- src/api/userService.js
+++ src/api/userService.js
@@ -5,17 +5,17 @@ import axios from 'axios'
 
 //אפשרות לכניסת משתמש
 export const login = (email,password) => {
-    return axios.post(`${baseUrl}api/user/logIn`,{email,password})
+    return axios.post(`${process.env.baseUrl}api/user/logIn`,{email,password})
 }
 
 //אפשרות להוספת משתמש
 export const signUp = (password,tz,email,name,role) => {
-    return axios.post(`${baseUrl}api/user`,{password,tz,email,name,role})
+    return axios.post(`${process.env.baseUrl}api/user`,{password,tz,email,name,role})
 }
 
 //קבלת כל המשתמשים
 export const getUser = () => {
-    return axios.get(`${baseUrl}api/user`)
+    return axios.get(`${process.env.baseUrl}api/user`)
 }
 
 
